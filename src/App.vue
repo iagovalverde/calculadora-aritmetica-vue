6c5118673908f200d729b6b895b699580a1f0344
Iago Valverde <iagopachiani96@gmail.com>
2023-11-01 11:17:10 -0300

    commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive({
+  operacao: '',
+  numero1: null,
+  numero2: null,
+})
+
+const calcularResultado = () => {
+  const { operacao, numero1, numero2} = estado;
+  
+  switch (operacao) {
+    case 'subtracao':
+      return numero1 - numero2;
+    case 'multiplicacao':
+      return numero1 * numero2;
+    case 'divisao':
+      return numero1 / numero2;
+    default:
+      return numero1 + numero2;
+    }
+}
+
+const atualizarOperacao = (e) => {
+  estado.operacao = e.target.value;
+}
+
+const atualizarNumero1 = (e) => {
+  estado.numero1 = parseFloat(e.target.value);
+}
+
+const atualizarNumero2 = (e) => {
+  estado.numero2 = parseFloat(e.target.value);
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1>Calculadora Aritmética</h1>
+    <div class="calc">
+      <input @keyup="atualizarNumero1" type="number" placeholder="Digite o primeiro numero">
+      <input @keyup="atualizarNumero2" type="number" placeholder="Digite o segundo numero">
+      <select @change="atualizarOperacao">
+        <option value="soma">+</option>
+        <option value="subtracao">-</option>
+        <option value="multiplicacao">x</option>
+        <option value="divisao">/</option>
+      </select>
+      <span>
+        {{ calcularResultado() }}
+      </span>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .calc, h1 {
+    text-align: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  input, select {
+    padding: 6px;
+    font-size: 20px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  span {
+    margin-left: 12px;
+    font-size: 20px;
   }
-}
 </style>
+
